c2bf2484c982542f469f8513934a122c981d3065
vodoleikin <vodoleikin@yandex.ru>
2025-09-27 14:16:57 +0300

    27.09.2025. 14.10. v14. All tests passed. Corrected comments on condition to added 'if settengs.DEBUD' in blogicum/blogicum/urls.py.

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'pages.apps.PagesConfig',
     "django_bootstrap5",
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -34,6 +35,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'blogicum.urls'
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -40,4 +40,12 @@ urlpatterns = [
         name='registration',
     ),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
